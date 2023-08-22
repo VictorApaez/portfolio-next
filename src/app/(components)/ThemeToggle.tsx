de8bc772315f2b3f118da6b5403cd02fb37c9bb8
VictorApaez <vic.apaez@gmail.com>
2023-08-22 13:38:01 -0400

    created button to toggle theme and store them in localStorage

---
+++ src/app/(components)/ThemeToggle.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useEffect } from "react";
+import { useTheme } from "../(context)/ThemeProvider";
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+    if (localTheme) {
+      setTheme(localTheme);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+    }
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="bg-button-primary text-white p-2 rounded"
+    >
+      Toggle {theme === "light" ? "Dark" : "Light"} Mode
+    </button>
+  );
+};
+
+export default ThemeToggle;
