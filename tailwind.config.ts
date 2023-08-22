54665c6b902d95115b45de1a692d66c12034a8e8
VictorApaez <vic.apaez@gmail.com>
2023-08-22 13:35:24 -0400

    extended the tailwind colors to include the global css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          1: "rgb(var(--primary-color-1))",
+          2: "rgb(var(--primary-color-2))",
+          3: "rgb(var(--primary-color-3))",
+        },
+        accent: {
+          1: "rgb(var(--accent-color-1))",
+          2: "rgb(var(--accent-color-2))",
+          3: "rgb(var(--accent-color-3))",
+        },
+        content: {
+          header: "rgb(var(--text-color-header))",
+          primary: "rgb(var(--text-color-primary))",
+        },
+        lines: {
+          primary: "rgb(var(--border-color-primary))",
+        },
+        button: {
+          primary: "rgb(var(--button-color-primary))",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
