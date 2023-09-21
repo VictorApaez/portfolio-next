2bfc4226e97e6aac5cce6d5180bd9f27eb621d49
VictorApaez <vic.apaez@gmail.com>
2023-09-21 11:34:15 -0400

    created navbar with href to sections

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
-  return <header>Header</header>;
+  const navItems = [
+    { path: "#home", name: "Home" },
+    { path: "#about", name: "About" },
+    { path: "#skills", name: "Skills" },
+    { path: "#projects", name: "Projects" },
+    { path: "#contact", name: "Contact" },
+  ];
+
+  return (
+    <header className="bg-primary-2 p-4 flex flex-row justify-center">
+      <nav>
+        <ul className="flex flex-row justify-center">
+          {navItems.map((item) => (
+            <li className="m-2">
+              <Link href={item.path}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
