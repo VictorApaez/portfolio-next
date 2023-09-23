7c50a8e198592bc7f03fc5dac09b15a50f9d14c5
VictorApaez <vic.apaez@gmail.com>
2023-09-23 16:02:33 -0400

    created a framer motion component for animating background

---
+++ src/components/Home/BackgroundLayer.tsx
@@ -0,0 +1,29 @@
+import { MotionValue, motion, motionValue } from "framer-motion";
+import React from "react";
+
+type BackgroundLayerProps = {
+  zIndex: number;
+  img: string;
+  y: MotionValue<string>;
+  bgPosition: string;
+};
+
+export const BackgroundLayer: React.FC<BackgroundLayerProps> = ({
+  zIndex,
+  img,
+  y,
+  bgPosition,
+}) => {
+  return (
+    <motion.div
+      className={`absolute inset-0 z-${zIndex}`}
+      style={{
+        backgroundImage: `url(/${img})`,
+        backgroundPosition: `${bgPosition}`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        y: y,
+      }}
+    />
+  );
+};
