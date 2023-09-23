2cad3e3cf3b91e8610c019cd5ed404fc2485b32a
VictorApaez <vic.apaez@gmail.com>
2023-09-23 16:04:48 -0400

    broke down component into seperate components

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,31 @@
 "use client";
+import useScrollAnimations from "@/hooks/useScrollAnimation";
+import { Content } from "./Content";
+import { BackgroundLayer } from "./BackgroundLayer";
 export default function Home() {
-  return <section id="home">Home</section>;
+  const { containerRef, backgroundY, textY, defaultY } = useScrollAnimations();
+
+  return (
+    <section
+      ref={containerRef}
+      id="home"
+      className=" h-screen relative overflow-hidden flex flex-col justify-between"
+    >
+      <Content y={textY} />
+
+      <BackgroundLayer
+        zIndex={0}
+        img={"back-layer.svg"}
+        y={backgroundY}
+        bgPosition={"55% 200px"}
+      />
+
+      <BackgroundLayer
+        zIndex={20}
+        img={"front-layer.svg"}
+        y={defaultY}
+        bgPosition={"55% 50px"}
+      />
+    </section>
+  );
 }
