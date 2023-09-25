df7e9ae3fc0b0c16c37f2654f1f1f7cfe03feb89
VictorApaez <vic.apaez@gmail.com>
2023-09-25 16:56:11 -0400

    created componenet for projects section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { projects } from "../../data/projects";
+import { ProjectDetails } from "./ProjectDetails";
 
 export default function Projects() {
-  return <section id="projects">Projects</section>;
+  return (
+    <section id="projects" className="p-10 flex flex-wrap justify-evenly">
+      {projects.map((project, id) => (
+        <ProjectDetails project={project} />
+      ))}
+    </section>
+  );
 }
