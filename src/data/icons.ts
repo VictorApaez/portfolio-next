358bbd905a41eed835ea4194ea11452cf43f2c2c
VictorApaez <vic.apaez@gmail.com>
2023-09-25 16:51:31 -0400

    created file for storing all icons

---
+++ src/data/icons.ts
@@ -0,0 +1,87 @@
+export type Icon = {
+  name: string;
+  url: string;
+  alt: string;
+};
+
+export type IconsObject = {
+  [key: string]: Icon;
+};
+
+export const iconsObject: IconsObject = {
+  html5: {
+    name: "HTML5",
+    url: "https://img.icons8.com/color/100/html-5--v1.png",
+    alt: "HTML5 Logo",
+  },
+  css3: {
+    name: "CSS3",
+    url: "https://img.icons8.com/color/100/css3.png",
+    alt: "CSS3 Logo",
+  },
+  javascript: {
+    name: "JavaScript",
+    url: "https://img.icons8.com/color/100/javascript.png",
+    alt: "JavaScript Logo",
+  },
+  python: {
+    name: "Python",
+    url: "https://img.icons8.com/color/100/python--v1.png",
+    alt: "Python Logo",
+  },
+  reactnative: {
+    name: "React Native",
+    url: "https://img.icons8.com/ios/100/react-native--v1.png",
+    alt: "React Native Logo",
+  },
+  next: {
+    name: "Next",
+    url: "https://img.icons8.com/fluency/100/000000/nextjs.png",
+    alt: "Next.js Logo",
+  },
+  react: {
+    name: "React",
+    url: "https://img.icons8.com/officel/100/react.png",
+    alt: "React Logo",
+  },
+  mongodb: {
+    name: "MongoDB",
+    url: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/100/external-mongodb-a-cross-platform-document-oriented-database-program-logo-color-tal-revivo.png",
+    alt: "MongoDB Logo",
+  },
+  express: {
+    name: "Express",
+    url: "https://img.icons8.com/officel/100/000000/express-js.png",
+    alt: "Express.js Logo",
+  },
+  nodejs: {
+    name: "Node JS",
+    url: "https://img.icons8.com/fluency/100/node-js.png",
+    alt: "Node.js Logo",
+  },
+  redux: {
+    name: "Redux",
+    url: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/100/external-redux-an-open-source-javascript-library-for-managing-application-state-logo-shadow-tal-revivo.png",
+    alt: "Redux Logo",
+  },
+  jest: {
+    name: "Jest",
+    url: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/100/external-jest-can-collect-code-coverage-information-from-entire-projects-logo-color-tal-revivo.png",
+    alt: "Jest Logo",
+  },
+  tailwind: {
+    name: "Tailwind",
+    url: "https://img.icons8.com/fluency/100/tailwind_css.png",
+    alt: "TailwindCSS Logo",
+  },
+  typescript: {
+    name: "TypeScript",
+    url: "https://img.icons8.com/fluency/48/typescript--v2.png",
+    alt: "TypeScript Logo",
+  },
+  jwt: {
+    name: "JWT",
+    url: "https://img.icons8.com/color/48/java-web-token.png",
+    alt: "JWT Logo",
+  },
+};
