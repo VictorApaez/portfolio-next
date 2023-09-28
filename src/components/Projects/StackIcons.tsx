f582535a51722af6e5934aba231e879349b9ab7a
VictorApaez <vic.apaez@gmail.com>
2023-09-28 11:03:17 -0400

    updated styles for project icons

--- src/components/Projects/StackIcons.tsx
+++ src/components/Projects/StackIcons.tsx
@@ -7,13 +7,13 @@ type StackIconsProp = {
 
 export const StackIcons: React.FC<StackIconsProp> = ({ icons }) => {
   return (
-    <ul className="flex flex-row">
+    <ul className="flex flex-row bg-primary-1 mb-4">
       {icons.map((icon, index) => {
         const { name, url, alt } = icon;
         return (
           <li className="m-2 flex flex-col place-items-center">
             <img src={url} alt={alt} className="w-8" />
-            <p>{name}</p>
+            <p className="text-sm">{name}</p>
           </li>
         );
       })}
