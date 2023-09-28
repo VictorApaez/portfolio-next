213b51c344ef5e704feca3b41b1f7e4d6d1bae30
VictorApaez <vic.apaez@gmail.com>
2023-09-28 10:52:04 -0400

    added skills component to about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ export default function Main() {
         }}
       />
       <About />
-      <Skills />
       <Projects />
       <Contact />
     </main>
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,23 +1,29 @@
 import React from "react";
+import Skills from "../Skills/Skills";
 
 export default function About() {
   return (
     <section id="about" className=" p-8">
-      <div className="container mx-auto max-w-screen-lg flex flex-wrap md:flex-nowrap space-y-8 md:space-y-0 md:space-x-8">
-        <div className="md:w-1/2 flex flex-col space-y-4">
-          <h1 className="text-3xl font-bold">About me</h1>
-          <p className=" leading-relaxed">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            eligendi maxime, minus corporis iste fugiat quisquam dolore aperiam.
-            Quas harum, laudantium excepturi asperiores distinctio pariatur
-            alias modi quibusdam sint a unde eos ipsam enim architecto.
-          </p>
+      <div className="flex justify-evenly">
+        <div className="flex flex-col w-1/2">
+          <div className=" flex flex-col space-y-4">
+            <h1 className="text-3xl font-bold">About me</h1>
+            <p className=" leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+              eligendi maxime, minus corporis iste fugiat quisquam dolore
+              aperiam. Quas harum, laudantium excepturi asperiores distinctio
+              pariatur alias modi quibusdam sint a unde eos ipsam enim
+              architecto.
+            </p>
+          </div>
+          <Skills />
         </div>
-        <div className="md:w-1/2">
+
+        <div className="">
           <img
             src="me-computer.PNG"
             alt="Victor Apaez"
-            className="w-full h-auto shadow-md rounded"
+            className="w-full h-auto"
           />
         </div>
       </div>
