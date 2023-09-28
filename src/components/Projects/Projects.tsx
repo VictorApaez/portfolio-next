d0719b36073a0fa2a2add369d3b04ee18344e973
VictorApaez <vic.apaez@gmail.com>
2023-09-28 11:02:44 -0400

    added header to projects section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,10 +4,16 @@ import { ProjectDetails } from "./ProjectDetails";
 
 export default function Projects() {
   return (
-    <section id="projects" className="p-10 flex flex-wrap justify-evenly">
-      {projects.map((project, id) => (
-        <ProjectDetails project={project} />
-      ))}
+    <section id="projects" className="p-20 my-32">
+      <div className="flex place-items-center mb-20">
+        <h1 className="text-5xl after:text-red-400">Things I've Built</h1>
+        <div className="ml-5 w-1/3 h-[2px] bg-accent-1"></div>
+      </div>
+      <div className="flex flex-wrap justify-between">
+        {projects.map((project, id) => (
+          <ProjectDetails project={project} />
+        ))}
+      </div>
     </section>
   );
 }
