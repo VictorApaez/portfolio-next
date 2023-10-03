08f3f39aa529a02959d3549e67f4f3e02345b4e6
VictorApaez <vic.apaez@gmail.com>
2023-10-03 08:48:39 -0400

    component for section headers

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type SectionHeaderProps = {
+  header: string;
+};
+
+export const SectionHeader: React.FC<SectionHeaderProps> = ({ header }) => {
+  return (
+    <div className="flex place-items-center mb-20 relative z-10 text-content-header">
+      <h1 className="text-5xl">{header}</h1>
+      <div className="ml-5 w-1/3 h-[3px] bg-accent-2"></div>
+    </div>
+  );
+};
