5dfddc33cb5e2c6f070ff1e98db7e52b7597a718
VictorApaez <vic.apaez@gmail.com>
2023-10-03 08:55:18 -0400

    created a new skills section that will display skills my category

--- src/screens/Skills/Skills.tsx
+++ src/screens/Skills/Skills.tsx
@@ -1,65 +1,66 @@
+import { SectionHeader } from "@/components/SectionHeader";
 import React from "react";
-import { Carousel } from "./Carousel";
+import { frontendIcons, backendIcons, devopsIcons } from "@/data/icons";
 
-export default function Skills() {
-  const icons = [
-    {
-      name: "HTML5",
-      url: "https://img.icons8.com/color/100/html-5--v1.png",
-    },
-    {
-      name: "CSS3",
-      url: "https://img.icons8.com/color/100/css3.png",
-    },
-
-    {
-      name: "JavaScript",
-      url: "https://img.icons8.com/color/100/javascript.png",
-    },
-    {
-      name: "Python",
-      url: "https://img.icons8.com/color/100/python--v1.png",
-    },
-    {
-      name: "React Native",
-      url: "https://img.icons8.com/ios/100/react-native--v1.png",
-    },
-    {
-      name: "Next",
-      url: "https://img.icons8.com/color/100/nextjs.png",
-    },
-    {
-      name: "React",
-      url: "https://img.icons8.com/officel/100/react.png",
-    },
-    {
-      name: "MongoDB",
-      url: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/100/external-mongodb-a-cross-platform-document-oriented-database-program-logo-color-tal-revivo.png",
-    },
-    {
-      name: "Express",
-      url: "https://img.icons8.com/color/100/express-js.png",
-    },
-    {
-      name: "Node JS",
-      url: "https://img.icons8.com/fluency/100/node-js.png",
-    },
-    {
-      name: "Redux",
-      url: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/100/external-redux-an-open-source-javascript-library-for-managing-application-state-logo-shadow-tal-revivo.png",
-    },
-    {
-      name: "Jest",
-      url: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/100/external-jest-can-collect-code-coverage-information-from-entire-projects-logo-color-tal-revivo.png",
-    },
-    {
-      name: "Tailwind",
-      url: "https://img.icons8.com/fluency/100/tailwind_css.png",
-    },
-  ];
+export const Skills = () => {
   return (
-    <section id="skills" className="flex justify-center w-full">
-      <Carousel icons={icons} />
+    <section className="bg-slate-600 p-12">
+      <SectionHeader header="Skills" />
+      <div className="flex justify-evenly">
+        <div className="bg-slate-500 p-5 rounded-xl w-full mx-5 my-10 flex flex-col items-center">
+          <div className="flex flex-col place-items-center bg-slate-400 p-4 rounded-lg -mt-20 w-fit">
+            <img
+              src="https://img.icons8.com/ios-filled/50/infinity.png"
+              alt="DevOps icon"
+            />
+            <h1 className="text-3xl text-content-header">DevOps</h1>
+          </div>
+          <ul className="w-full flex flex-wrap justify-between">
+            {backendIcons.map((icon, i) => (
+              <li className="flex items-center m-5 min-w-1/4 max-w-1/3">
+                <img src={icon.url} className="h-8 mr-4" />
+                <p className="text-lg">{icon.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="bg-slate-500 p-5 rounded-xl w-full mx-5 flex flex-col items-center">
+          <div className="flex flex-col justify-center place-items-center bg-slate-400 p-4 rounded-lg -mt-20 w-fit">
+            <img
+              src="https://img.icons8.com/pastel-glyph/64/code--v2.png"
+              alt="front end icon"
+            />
+            <h1 className="text-3xl text-content-header">Front-End</h1>
+          </div>
+
+          <ul className="w-full flex flex-wrap">
+            {frontendIcons.map((icon, i) => (
+              <li className="flex m-5">
+                <img src={icon.url} className="h-8 mr-4" />
+                <p className="text-lg">{icon.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="bg-slate-500 p-5 rounded-xl w-full mx-5 my-10 flex flex-col items-center">
+          <div className="flex flex-col justify-center place-items-center bg-slate-400 p-4 rounded-lg -mt-20 w-fit">
+            <img
+              src="https://img.icons8.com/pulsar-line/64/backend-development.png"
+              alt="backend icon"
+            />
+            <h1 className="text-3xl text-content-header">Back-End</h1>
+          </div>
+          <ul className="w-full flex flex-wrap">
+            {backendIcons.map((icon, i) => (
+              <li className="flex m-5">
+                <img src={icon.url} className="h-8 mr-4" />
+                <p className="text-lg">{icon.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </section>
   );
-}
+};
