90c94b50a596d8e80b6460a4394d9daa98fb7a05
VictorApaez <vic.apaez@gmail.com>
2023-10-03 08:51:34 -0400

    removed hard coded divider and added skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
+"use client";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 import About from "@/screens/About/About";
 import Contact from "@/screens/Contact/Contact";
 import Home from "@/screens/Home/Home";
 import Projects from "@/screens/Projects/Projects";
-import Skills from "@/screens/Skills/Skills";
+import { ParallaxDivider } from "@/components/ParallaxDivider";
+import { Skills } from "@/screens/Skills/Skills";
+
 export default function Main() {
   return (
     <main>
       <Home />
-      <div
-        className="z-40 h-28"
-        style={{
-          background: "linear-gradient(to bottom, #002A28, transparent)",
-        }}
-      />
       <About />
+      <Skills />
       <Projects />
       <Contact />
     </main>
