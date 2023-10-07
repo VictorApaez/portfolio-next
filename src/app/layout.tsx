fc5c08900b4785b720f2c611020b502ba803dde3
VictorApaez <vic.apaez@gmail.com>
2023-10-06 21:25:40 -0400

    added componenet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "@/screens/Header/Header";
 import Footer from "@/screens/Footer/Footer";
 import ColorPalette from "@/screens/ColorPalette";
 import { SocialsBar } from "@/components/SocialsBar";
+import { FixedContent } from "@/components/FixedContent";
 
 export default function RootLayout({
   children,
@@ -17,10 +18,9 @@ export default function RootLayout({
     <html lang="en">
       <ThemeProvider>
         <SocialsBar />
+        <FixedContent />
         <Header />
         {children}
-        <Footer />
-        <ColorPalette />
       </ThemeProvider>
     </html>
   );
