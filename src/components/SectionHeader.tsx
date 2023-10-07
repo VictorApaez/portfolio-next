ec908e8837c7ff0449926159d7af55554063b187
VictorApaez <vic.apaez@gmail.com>
2023-10-06 21:27:09 -0400

    updated styles

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -6,9 +6,9 @@ type SectionHeaderProps = {
 
 export const SectionHeader: React.FC<SectionHeaderProps> = ({ header }) => {
   return (
-    <div className="flex place-items-center mb-20 ml-20 relative z-10 text-content-header">
-      <h1 className="text-5xl">{header}</h1>
-      <div className="ml-5 w-1/3 h-[3px] bg-accent-2"></div>
+    <div className="flex place-items-center md:ml-20 ml-4 relative z-10 text-content-header">
+      <h1 className="text-5xl md:text-5xl">{header}</h1>
+      <div className="ml-2 md:ml-5 w-1/4 md:w-1/3 h-[3px] bg-accent-2"></div>
     </div>
   );
 };
