82088afab6cac85ab6245c67bafa16fa2f51bbd7
VictorApaez <vic.apaez@gmail.com>
2023-10-06 21:24:24 -0400

    created email content fixed to bottom right

---
+++ src/components/FixedContent.tsx
@@ -0,0 +1,11 @@
+export const FixedContent = () => {
+  return (
+    <div
+      className="fixed z-50 bottom-1/4 -right-32 flex place-items-center"
+      style={{ transform: "rotate(90deg)" }}
+    >
+      <p className="p-4 text-lg">vic.apaez@gmail.com</p>
+      <div className="border-t-2 border-white w-32"></div>
+    </div>
+  );
+};
