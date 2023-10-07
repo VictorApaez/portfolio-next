a07be697ba20196bdfeafb00b32196eed7cd02a1
VictorApaez <vic.apaez@gmail.com>
2023-10-06 21:26:43 -0400

    added email to  socials

--- src/components/SocialsBar.jsx
+++ src/components/SocialsBar.jsx
@@ -9,35 +9,62 @@ export const SocialsBar = () => {
       img: "https://img.icons8.com/color/48/linkedin.png",
       link: "https://www.linkedin.com/in/victor-apaez/",
       alt: "linkedin",
+      mailto: false,
     },
     {
       name: "Github",
       img: "https://img.icons8.com/sf-black-filled/48/github.png",
       alt: "Github",
       link: "https://github.com/VictorApaez",
+      mailto: false,
+    },
+    {
+      name: "Email",
+      img: "https://img.icons8.com/plasticine/48/new-post--v2.png",
+      alt: "email",
+      link: "mailto:example@example.com?subject=Question from Website&body=Hello, I have a question about...",
+      mailto: true,
     },
   ];
 
   return (
     <div className="fixed inset-0 z-50 flex items-center max-w-fit">
       <section className={` rounded-tr-lg rounded-br-lg bg-slate-200`}>
-        {icons.map((icon, i) => (
-          <Link href={icon.link} target="_blank" key={i}>
-            <motion.img
-              src={icon.img}
-              alt={icon.alt}
-              className={`p-1 bg-slate-200 ${i === 0 ? "rounded-tr-lg" : ""} ${
-                i === icons.length - 1 ? "rounded-br-lg" : ""
-              }`}
-              whileHover={{
-                x: 15,
-                borderTopRightRadius: 8,
-                borderBottomRightRadius: 8,
-              }} // Translates the image 10 pixels to the right on hover.
-              transition={{ duration: 0.2 }} // Duration for the hover effect.
-            />
-          </Link>
-        ))}
+        {icons.map((icon, i) =>
+          icon.mailto ? (
+            <a href={icon.link} key={i}>
+              <motion.img
+                src={icon.img}
+                alt={icon.alt}
+                className={`p-1 bg-slate-200 ${
+                  i === 0 ? "rounded-tr-lg" : ""
+                } ${i === icons.length - 1 ? "rounded-br-lg" : ""}`}
+                whileHover={{
+                  x: 15,
+                  borderTopRightRadius: 8,
+                  borderBottomRightRadius: 8,
+                }}
+                transition={{ duration: 0.2 }}
+              />
+            </a>
+          ) : (
+            <Link href={icon.link} key={i}>
+              <motion.img
+                src={icon.img}
+                alt={icon.alt}
+                className={`p-1 bg-slate-200 ${
+                  i === 0 ? "rounded-tr-lg" : ""
+                } ${i === icons.length - 1 ? "rounded-br-lg" : ""}`}
+                whileHover={{
+                  x: 15,
+                  borderTopRightRadius: 8,
+                  borderBottomRightRadius: 8,
+                }}
+                transition={{ duration: 0.2 }}
+              />
+            </Link>
+          )
+        )}
       </section>
     </div>
   );
