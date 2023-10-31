d3a8ea7b9fd522e187d223bfce1ef1e281f5ff2e
VictorApaez <vic.apaez@gmail.com>
2023-10-31 19:24:04 -0400

    deleted route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-import { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
 import About from "@/screens/About/About";
 import Contact from "@/screens/Contact/Contact";
 import Home from "@/screens/Home/Home";
 import Projects from "@/screens/Projects/TopFourProjects";
-import { ParallaxDivider } from "@/components/ParallaxDivider";
 import { Skills } from "@/screens/Skills/Skills";
+import AllProjects from "../screens/Projects/AllProjects";
 
 export default function Main() {
   return (
@@ -15,6 +13,7 @@ export default function Main() {
       <About />
       <Skills />
       <Projects />
+      <AllProjects />
       <Contact />
     </main>
   );
