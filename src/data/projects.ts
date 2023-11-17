cc4f60e883c14ac3170a296e70d95b88214607ea
VictorApaez <56009643+VictorApaez@users.noreply.github.com>
2023-11-17 08:49:12 -0500

    Update LoL demo link

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -63,7 +63,7 @@ export const projects: ProjectType[] = [
     isLive: true,
   },
   {
-    demoLink: "https://legendsofleague.netlify.app/",
+    demoLink: "legendary-slayers-fe.vercel.app",
     githubLink: "https://github.com/durlinlin/Legendary-Slayers-FE",
     imgUrl: "LoL.png",
     alt: "Image of the web app League of Legends",
